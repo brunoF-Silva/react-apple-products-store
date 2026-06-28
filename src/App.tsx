293166fa7eaf5c8cab6fbfd3109123da6e447b8a
Bruno Silva <92815660+brunoF-Silva@users.noreply.github.com>
2026-06-28 17:47:10 -0300

    feat: add home page
    Co-authored-by: silva.bruno1 <silva.bruno1@uft.edu.br>

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react'
 import Controls from './components/Controls'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 const App = () => {
   const [frameZoom, setFrameZoom] = useState(false)
@@ -20,9 +21,12 @@ const App = () => {
         frameZoom && 'min-w-[97vw] min-h-[97vh]'
         } w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw]
         max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative
-        transition-all duration-100`}>
-          <Navbar activePage={activePage} handleNavClick={handleNavClick}/>
+        transition-all duration-100 flex`}>
+        <Navbar activePage={activePage} handleNavClick={handleNavClick}/>
         <Controls toggleZoom={toggleZoom} frameZoom={frameZoom}/>
+        <div className="flex-grow">
+          <Home/>
+        </div>
       </div>
     </div>
   )
